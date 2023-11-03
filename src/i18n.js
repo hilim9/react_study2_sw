8d5c4ae7caf30d459c3769eaf23163810f06b5db
hilim9 <dlagydls1212@naver.com>
2023-11-03 12:58:13 +0900

    'react_project'

---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import ko from './langs/ko';
+import en from './langs/en';
+
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ko: {
+    translation: ko,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ko',
+});
+
+export default i18n;
